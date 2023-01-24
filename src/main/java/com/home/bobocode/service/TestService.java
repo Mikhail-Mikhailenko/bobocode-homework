a2a22556b82ff7890d10e5692b2b50c5769de614
Mikhail Mikhailenko <mmikhailenko@ibitworks.com>
2023-01-24 23:09:54 +0200

    Bring context * Scan beans by package path * Initialize objects and put them to context * Add methods for get beans

---
+++ src/main/java/com/home/bobocode/service/TestService.java
@@ -0,0 +1,11 @@
+package com.home.bobocode.service;
+
+import com.home.bobocode.annotations.Bean;
+
+@Bean()
+public class TestService {
+
+    public void test() {
+        System.out.println("Test!!!");
+    }
+}
